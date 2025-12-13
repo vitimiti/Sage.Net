f07116b6a79d33d0b5414c5ea14998f0a4b6405e
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-13 03:41:31 +0000

    Add `IniLoadType` enum in `GameEngine.Common`.
    - Introduce `IniLoadType` enumeration with `Invalid`, `Overwrite`, `CreateOverrides`, and `Multifile` load types.
    - Include XML documentation for all load types.
    
    Signed-off-by: Víctor Matía Rodríguez <vmatir@outlook.com>

---
+++ Sage.Net.GameEngine/Common/IniLoadType.cs
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------------------
+// <copyright file="IniLoadType.cs" company="Sage.Net">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 Sage.Net Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Sage.Net.GameEngine.Common;
+
+/// <summary>The behavior of loading the INI data into items.</summary>
+public enum IniLoadType
+{
+    /// <summary>Invalid load type.</summary>
+    Invalid,
+
+    /// <summary>Create new or load <b>over</b> existing data instance.</summary>
+    Overwrite,
+
+    /// <summary>Create new or load into <b>new</b> override data instance.</summary>
+    CreateOverrides,
+
+    /// <summary>Create new or continue loading into existing data instance.</summary>
+    Multifile,
+}

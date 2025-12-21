bc8d834a641d866ea09a46c74c4e4811422f278b
Víctor Matía Rodríguez <vmatir@outlook.com>
2025-12-21 11:53:27 +0000

    Add SDL3-based `SdlSplashScreen` implementation and API binding.
    - Introduce `SdlSplashScreen` for displaying a splash screen using SDL3.
    - Create `Sdl` static class with SDL3 API bindings for window and surface management.
    - Add project file `Sage.Net.Sdl3.csproj` with dependencies and references.
    
    Signed-off-by: Víctor Matía Rodríguez <vmatir@outlook.com>

---
+++ Sage.Net.Sdl3/Sdl.cs
@@ -0,0 +1,201 @@
+// -----------------------------------------------------------------------
+// <copyright file="Sdl.cs" company="Sage.Net">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 Sage.Net Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Sage.Net.Sdl3;
+
+internal static unsafe partial class Sdl
+{
+    public const uint InitVideo = 0x0020;
+
+    public static string? GetError => Utf8StringMarshaller.ConvertToManaged(GetErrorUnsafe());
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_Init")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    [return: MarshalAs(UnmanagedType.I4)]
+    public static partial bool Init(uint flags);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_Quit")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    public static partial void Quit();
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_PollEvent")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    [return: MarshalAs(UnmanagedType.I4)]
+    public static partial bool PollEvent(out SdlEvent sdlEvent);
+
+    [NativeMarshalling(typeof(SafeHandleMarshaller<Surface>))]
+    public sealed class Surface : SafeHandle
+    {
+        private NativeSurface* UnsafeHandle => (NativeSurface*)handle;
+
+        public Surface()
+            : base(invalidHandleValue: nint.Zero, ownsHandle: true) => SetHandle(nint.Zero);
+
+        public Surface(bool ownsHandle)
+            : base(invalidHandleValue: nint.Zero, ownsHandle: ownsHandle) => SetHandle(nint.Zero);
+
+        public static Surface FromBmp(string file) => LoadBmp(file);
+
+        public static bool Blit(Surface src, Surface dst) => BlitSurface(src, null, dst, null);
+
+        public override bool IsInvalid => handle == nint.Zero;
+
+        public int Width => UnsafeHandle->W;
+
+        public int Height => UnsafeHandle->H;
+
+        protected override bool ReleaseHandle()
+        {
+            if (IsInvalid)
+            {
+                return true;
+            }
+
+            DestroySurface(handle);
+            SetHandle(nint.Zero);
+            return true;
+        }
+    }
+
+    [NativeMarshalling(typeof(SafeHandleMarshaller<Window>))]
+    public sealed class Window : SafeHandle
+    {
+        public const ulong Borderless = 0x0010;
+
+        public Window()
+            : base(invalidHandleValue: nint.Zero, ownsHandle: true) => SetHandle(nint.Zero);
+
+        public override bool IsInvalid => handle == nint.Zero;
+
+        public Surface Surface
+        {
+            get
+            {
+                var surfaceHandle = GetWindowSurface(this);
+                Surface surface = new(ownsHandle: false);
+                if (surfaceHandle == nint.Zero)
+                {
+                    return surface;
+                }
+
+                Marshal.InitHandle(surface, surfaceHandle);
+                return surface;
+            }
+        }
+
+        public static Window Create(string title, int width, int height, ulong flags) =>
+            CreateWindow(title, width, height, flags);
+
+        public bool UpdateSurface() => UpdateWindowSurface(this);
+
+        protected override bool ReleaseHandle()
+        {
+            if (IsInvalid)
+            {
+                return true;
+            }
+
+            DestroyWindow(handle);
+            SetHandle(nint.Zero);
+            return true;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+        public int X;
+        public int Y;
+        public int W;
+        public int H;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct SdlEvent
+    {
+        [FieldOffset(0)]
+        public uint Type;
+
+        [FieldOffset(0)]
+        public fixed byte Padding[128];
+    }
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_GetError")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial byte* GetErrorUnsafe();
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_LoadBMP", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial Surface LoadBmp(string file);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_BlitSurface")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    [return: MarshalAs(UnmanagedType.I4)]
+    private static partial bool BlitSurface(Surface src, Rect* srcRect, Surface dst, Rect* dstRect);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_DestroySurface")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial void DestroySurface(nint window);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_CreateWindow", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial Window CreateWindow(string title, int width, int height, ulong flags);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_GetWindowSurface", StringMarshalling = StringMarshalling.Utf8)]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial nint GetWindowSurface(Window window);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_UpdateWindowSurface")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    [return: MarshalAs(UnmanagedType.I4)]
+    private static partial bool UpdateWindowSurface(Window window);
+
+    [LibraryImport("SDL3", EntryPoint = "SDL_DestroyWindow")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static partial void DestroyWindow(nint window);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct NativeSurface
+    {
+        public uint Flags;
+        public uint Format;
+        public int W;
+        public int H;
+        public int Pitch;
+        public nint Pixels;
+        public int RefCount;
+        public nint Reserved;
+    }
+}
---
+++ Sage.Net.Sdl3/SdlSplashScreen.cs
@@ -0,0 +1,176 @@
+// -----------------------------------------------------------------------
+// <copyright file="SdlSplashScreen.cs" company="Sage.Net">
+// A transliteration and update of the CnC Generals (Zero Hour) engine and games with mod-first support.
+// Copyright (C) 2025 Sage.Net Contributors
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see https://www.gnu.org/licenses/.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
+using Sage.Net.Abstractions;
+using Sage.Net.LoggerHelper;
+
+namespace Sage.Net.Sdl3;
+
+/// <inheritdoc/>
+public sealed partial class SdlSplashScreen(ILogger<SdlSplashScreen> logger) : ISplashScreen
+{
+    private Sdl.Window? _window;
+    private Sdl.Surface? _windowSurface;
+    private Sdl.Surface? _bmp;
+
+    /// <inheritdoc/>
+    public bool InitializationIsComplete { get; set; }
+
+    /// <summary>
+    /// Initializes and sets up the splash screen using the provided base game path and game options.
+    /// </summary>
+    /// <param name="baseGamePath">
+    /// The root directory of the game, used to locate the default splash screen bitmap if a mod-specific file is not provided.
+    /// </param>
+    /// <param name="options">
+    /// A <see cref="GameOptions"/> object containing configuration settings for the splash screen,
+    /// including the optional mod files path and splash screen bitmap file name.
+    /// </param>
+    public void Initialize(string baseGamePath, GameOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        using IDisposable? logContext = LogContext.BeginOperation(logger, nameof(Initialize));
+
+        if (!Sdl.Init(Sdl.InitVideo))
+        {
+            var error = Sdl.GetError ?? "Unknown error";
+            Log.SdlInitializationFailed(logger, error);
+
+            throw new InvalidOperationException($"SDL initialization failed ({error}).");
+        }
+
+        var bitmapPath = string.Empty;
+        if (
+            options.ModFilesPath is not null
+            && options.ModSplashScreenBmpFileName is not null
+            && Directory.Exists(options.ModFilesPath)
+        )
+        {
+            var tempPath = Path.Combine(options.ModFilesPath, options.ModSplashScreenBmpFileName);
+            if (File.Exists(tempPath))
+            {
+                bitmapPath = tempPath;
+            }
+        }
+
+        if (string.IsNullOrWhiteSpace(bitmapPath))
+        {
+            // This is the actual shipped splash screen bitmap.
+            bitmapPath = Path.Combine(baseGamePath, "Install_Final.bmp");
+        }
+
+        Log.UsingBitmapFile(logger, bitmapPath);
+
+        _bmp = Sdl.Surface.FromBmp(bitmapPath);
+        if (_bmp.IsInvalid)
+        {
+            Log.FailedToLoadBitmapFile(logger, bitmapPath);
+            return;
+        }
+
+        _window = Sdl.Window.Create($"{options.WindowTitle} - Loading", _bmp.Width, _bmp.Height, Sdl.Window.Borderless);
+        if (_window.IsInvalid)
+        {
+            Log.FailedToCreateWindow(logger, Sdl.GetError ?? "Unknown error");
+            return;
+        }
+
+        _windowSurface = _window.Surface;
+        if (_windowSurface.IsInvalid)
+        {
+            Log.FailedToGetWindowSurface(logger, Sdl.GetError ?? "Unknown error");
+            return;
+        }
+
+        _ = Sdl.Surface.Blit(_bmp, _windowSurface);
+        _ = _window.UpdateSurface();
+    }
+
+    /// <summary>
+    /// Processes pending SDL events in the event queue to ensure the application remains responsive
+    /// and properly handles windowing, particularly on environments such as Linux with Wayland.
+    /// </summary>
+    public void Update()
+    {
+        while (Sdl.PollEvent(out _))
+        {
+            // Keep polling events
+            // This is ESSENTIAL for systems like Linux on Wayland that require event
+            // handling to show a window.
+        }
+    }
+
+    /// <summary>
+    /// Renders the splash screen by blitting the bitmap surface onto the window surface.
+    /// </summary>
+    /// <remarks>
+    /// This method verifies the validity of the window, window surface, and bitmap surface before attempting
+    /// the rendering operation. If any of these surfaces are invalid, the method exits without performing any rendering.
+    /// </remarks>
+    public void Draw()
+    {
+        if (_window?.IsInvalid == true || _windowSurface?.IsInvalid == true || _bmp?.IsInvalid == true)
+        {
+            return;
+        }
+
+        _ = _window!.UpdateSurface();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        _bmp?.Dispose();
+        _windowSurface?.Dispose();
+        _window?.Dispose();
+
+        // TODO: Move this to the main loop system (window system maybe?)
+        Sdl.Quit();
+    }
+
+    private static partial class Log
+    {
+        [LoggerMessage(
+            LogLevel.Critical,
+            Message = "SDL cannot be initialized and the game will now terminate ({Error})."
+        )]
+        public static partial void SdlInitializationFailed(ILogger logger, string error);
+
+        [LoggerMessage(LogLevel.Debug, Message = "Using bitmap file: {BitmapPath}")]
+        public static partial void UsingBitmapFile(ILogger logger, string bitmapPath);
+
+        [LoggerMessage(LogLevel.Error, Message = "Failed to load bitmap file: {BitmapPath}")]
+        public static partial void FailedToLoadBitmapFile(ILogger logger, string bitmapPath);
+
+        [LoggerMessage(
+            LogLevel.Error,
+            Message = "Failed to create window ({Error}), the splash screen will not be displayed."
+        )]
+        public static partial void FailedToCreateWindow(ILogger logger, string error);
+
+        [LoggerMessage(
+            LogLevel.Error,
+            Message = "Failed to get the window surface ({Error}), the splash screen will not be displayed."
+        )]
+        public static partial void FailedToGetWindowSurface(ILogger logger, string error);
+    }
+}
